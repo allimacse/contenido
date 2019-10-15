ccbabca564cf401994dd7e8e446718488909dc8e
allimacse <alan.escamillam@gmail.com>
2019-10-15 14:14:30 -0500

    Limpieza de codigo comentado en plantilla.php

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -15,7 +15,6 @@
     <link href="<?php echo SERVERURL; ?>vistas/css/animate.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo SERVERURL; ?>vistas/css/animate.min.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo SERVERURL; ?>vistas/css/header.css" rel="stylesheet" type="text/css" />
-    <!--<link href="<?php echo SERVERURL; ?>vistas/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>-->
     <script src="<?php echo SERVERURL; ?>vistas/js/validarRegistro.js" type="text/javascript"></script>
     <script src="<?php echo SERVERURL; ?>vistas/js/jquery-3.1.1.min.js"></script>
     <script src="<?php echo SERVERURL; ?>vistas/js/sweetalert2.min.js"></script>
@@ -24,9 +23,6 @@
     <script src="<?php echo SERVERURL; ?>vistas/js/ripples.min.js"></script>
     <script src="<?php echo SERVERURL; ?>vistas/js/jquery.mCustomScrollbar.concat.min.js"></script>
     <script src="<?php echo SERVERURL; ?>vistas/js/main.js"></script>
-    <!--        <link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" />
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>-->
     <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.1/themes/base/jquery-ui.css" />
     <script src="http://code.jquery.com/ui/1.10.1/jquery-ui.js"></script>
     <script src="https://momentjs.com/downloads/moment.js"></script>
@@ -45,12 +41,10 @@
     $vistasR = $vt->obtener_vistas_controlador();
     if ($vistasR == "login" || $vistasR == "404") :
         if ($vistasR == "login") {
-
             require_once "./vistas/contenidos/login-vistas.php";
         } else {
             require_once "./vistas/contenidos/404-vistas.php";
         } else :
-        //session_start(['name' => 'SDO']);
         require_once "./controladores/logincontrolador.php";
 
         $lc = new loginControlador();
